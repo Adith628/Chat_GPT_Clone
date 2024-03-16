bef9abaee9b35ef4fd8546c867fc8b1b087b25b3
Adithyan T <adithsreedhar628@gmail.com>
2024-03-16 16:28:38 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="text-white flex bg-gray-950">
+    <div className="text-white flex h-screen overflow-y-hidden bg-gray-950">
       <Main />
     </div>
   );
